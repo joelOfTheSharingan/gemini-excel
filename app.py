dcc668282fa7189997344d686c7513607729f3e3
Joel Michael George <joelmgeorge.2010@icloud.com>
2025-08-14 18:40:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ def upload_pdf():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
